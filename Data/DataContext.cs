5044adc0c2d7c2b96fec7b9ee97a7e32ff55d224
Danilo Santana <danilosantana240765@gmail.com>
2022-09-27 22:27:52 -0300

    Adicionar arquivos de projeto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCRUD.Data {
+	public class DataContext  : DbContext {
+		public DataContext(DbContextOptions<DataContext> options) : base(options) 
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Game>().HasData(
+				new Game { Id = 1, Name = "Half Life 2", Developer = "Valve", Release = new DateTime(2004, 10, 10) },
+				new Game { Id = 2, Name = "Day of the Tentacle", Developer = "Lucas Arts", Release = new DateTime(2002, 08, 10) }
+			);
+		}
+		public DbSet<Game> Games => Set<Game>();
+	}
+}
---
+++ Data/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServerCRUD.Data {
+	public class Game {
+		public int Id { get; set; }	
+		public string Name { get; set; }	
+		public string Developer { get; set; }
+		public DateTime? Release { get; set; }
+	}
+}
